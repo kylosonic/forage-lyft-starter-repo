69af5e6c811092ddeb8884d020b00978cd1010fa
Nahom Girma Woldemichael <119425731+NahomGirmaWoldemichael@users.noreply.github.com>
2023-12-02 14:58:27 +0300

    adlkv

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,24 +1,12 @@
-from datetime import datetime
- 
-from abc import ABC
-
-from car import Car
+from engine.engine import Engine
 
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
+class SternmanEngine(Engine):
+    def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
-
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        if self.warning_light_is_on:
             return True
         else:
             return False
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,22 +1,10 @@
-from datetime import datetime
-
-from abc import ABC
-
-from car import Car
+from engine.engine import Engine
 
 
-class WilloughbyEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+class WilloughbyEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 60000
-
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return self.current_mileage - self.last_service_mileage > 60000
